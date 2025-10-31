491ec3df4394f54e11971dcda94eadc6d08c1842
RichieChen2004 <133861266+RichieChen2004@users.noreply.github.com>
2025-10-31 19:38:55 +1100

    Protoyped actions bar

--- graph-vis/src/app/app.ts
+++ graph-vis/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Board } from './components/board/board';
+import { ActionBar } from './components/action-bar/action-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Board],
+  imports: [RouterOutlet, Board, ActionBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ graph-vis/src/app/components/action-bar/action-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-action-bar',
+  imports: [],
+  templateUrl: './action-bar.html',
+  styleUrl: './action-bar.scss',
+})
+export class ActionBar {
+
+}
